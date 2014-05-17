b05a4d9255875f8dd8f613cbc76e888926019d8c
Henri Bergius <henri.bergius@iki.fi>
2014-05-17 03:06:44 +0200

    CoffeeScript register

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var coffeeScript = require('coffee-script');
+if (typeof coffeeScript.register !== 'undefined') {
+  coffeeScript.register();
+}
 var tester = require('./lib/test.coffee');
 exports.component = tester.component;
